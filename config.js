ef67b831f6fedcf70bed6f567be2f26e22a314a9
dangyours <48520458+dangyours@users.noreply.github.com>
2019-10-13 16:06:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// birthday
 var config = {
-    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 句子的长度可以任意，
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",
@@ -15,15 +15,15 @@ var config = {
         "守护在你身边的！",
         "爱你的 东~"
     ],
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    // 按钮文字描述, 以下是默认的按钮文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "快点我~",
+        play: "听点歌吧",
+        bannar_coming: "生日快乐",
+        balloons_flying: "气球哟",
+        cake_fadein: "不会胖的蛋糕",
+        light_candle: "点根蜡烛",
+        wish_message: "生日快乐~",
+        story: "最后...",
     }
 };
